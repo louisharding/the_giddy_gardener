82413eab80625ee46fb7ed9fd1589a164acfb9a1
louisharding <lgharding0@gmail.com>
2025-11-14 23:39:51 +0000

    feat: refactor index templates and update URL routing for crops

--- growing_projects/models.py
+++ growing_projects/models.py
@@ -2,23 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+TYPE_CHOICES = [
+    ("leaf", "Leaf"),
+    ("root", "Root"),
+    ("tuber", "Tuber"),
+    ("stalk", "Stalk"),
+    ("fruit", "Fruit"),
+    ("mushroom", "Mushroom"),
+    ("seed", "Seed"),
+    ("cereal", "Cereal"),
+    ("legume", "Legume"),
+    ("nut", "Nut"),
+    ("spice", "Spice"),
+]
 
 class Crop(models.Model):
-    TYPE_CHOICES = [
-        ("leaf", "Leaf"),
-        ("root", "Root"),
-        ("tuber", "Tuber"),
-        ("stalk", "Stalk"),
-        ("fruit", "Fruit"),
-        ("mushroom", "Mushroom"),
-        ("seed", "Seed"),
-        ("cereal", "Cereal"),
-        ("legume", "Legume"),
-        ("nut", "Nut"),
-        ("spice", "Spice"),
-    ]
     name = models.CharField(max_length=200, unique=True)
     type = models.CharField(choices=TYPE_CHOICES)
+    """
     sowing_range = models.DateField()
     harvestring_range = models.DateField()
     LIFE_CYCLE_CHOICES = (
@@ -27,29 +28,30 @@ class Crop(models.Model):
         ("many_years", "Many Years"),
     )
     life_cycle = models.CharField(max_length=20, choices=LIFE_CYCLE_CHOICES)
-
-    #add method for returning sowing ranges and harvesting ranges
-    #add method for returning season range 
+    add method for returning sowing ranges and harvesting ranges
+    add method for returning season range 
+    """
 
   
 class Allotment(models.Model):
     name = models.CharField(max_length=200, default="Allotment")
     garden = models.CharField(max_length=200, default="garden")
-    # area = "" # in meters square
-    # soilTypes = "" #inclusive dropdown list
-    # climate = "" #dropdownlist
-    # coverage = "" #dropdown list eg eg- indoor, outdoor, greenhouse, coldframe etc
-    # seaLevel = ""
-    # country = ""
-    # city = ""
-    # localPests = ""
-    # soilAcidity = ""
-    # postcode = ""
-    #perhaps an option to make an allotment public, doing so allows users to create premade common allotments for other users to 
-    #select from and change as they see fit
-    #there would need to be a limit as to how many public allotments could be made by a user
-    #as to avoid griefing and spamming
-
+    """
+    area = "" # in meters square
+    soilTypes = "" #inclusive dropdown list
+    climate = "" #dropdownlist
+    coverage = "" #dropdown list eg eg- indoor, outdoor, greenhouse, coldframe etc
+    seaLevel = ""
+    country = ""
+    city = ""
+    localPests = ""
+    soilAcidity = ""
+    postcode = ""
+    perhaps an option to make an allotment public, doing so allows users to create premade common allotments for other users to 
+    select from and change as they see fit
+    there would need to be a limit as to how many public allotments could be made by a user
+    as to avoid griefing and spamming
+    """
 
 # Each user can add a garden, a garden can contain multiple allotments,
 # A garden is simply where a user stores their saved allotments
--- growing_projects/urls.py
+++ growing_projects/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('plants', views.CropList.as_view(), name='plants'),
+    # path('<name:name>/', views.crop_detail, name='crop_detail'),
     #path for garden
-    # 
-
 ]
